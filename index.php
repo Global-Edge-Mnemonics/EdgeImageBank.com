f00165a222201e58974bb2bd8e2a3a3895fc7b81
Tom Norwood <tommygun@mnemonics.co.uk>
2022-07-19 14:33:29 +0100

    Initialising Repo

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+"http://www.w3.org/TR/html4/loose.dtd">
+<?php date_default_timezone_set('Europe/London'); ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="imagetoolbar" content="no" />
+<title>Edge Image Bank  . .  analogue and digital arts</title>
+</head>
+ 
+<body bgcolor="#ffffff" text="#003366" link="#006699" vlink="#006699" alink="#336699" topmargin="0" leftmargin="0">
+ <table width="100%" height="400" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+   <td width="100%" height="50%" align="right">
+    <img src="/coming-soon/images/clear.gif" width="1" height="100" /></td>
+  </tr><tr>
+   <td width="100%" align="center">
+    <table width="416" border="0" cellpadding="0" cellspacing="0">
+     <tr>
+      <td width="100%">
+       <img src="/coming-soon/images/edgecard.gif" width="416" height="275" border="0" alt="Edge Image Bank.com - analogue and digital arts" title="Edge Image Bank.com - analogue and digital arts" /></td>
+     </tr><tr>
+      <td align="center">
+       <font face="Verdana, Arial" size="1">
+        <br />
+        <br />
+        <b>EdgeImageBank Pictures</b><br />
+        <br />
+        For all the films and TV shows, EdgeImageBank database, news and update register to <a href="http://www.diaryunlimited.com/" target="_blank">DiaryUnlimited</a><br />
+        <br />
+        <br />
+        <br />
+        All content and trademarks &copy; <?php print date('Y'); ?> EdgeImageBank Pictures and <br>GEM Entertainment, New York. <a href="mailto:info@mnemonics.co.uk?subject=edge.site" title="contact us">Contact</a><br />
+       </font></td>
+     </tr>
+    </table></td>
+  </tr><tr>
+   <td width="100%" height="50%" align="right">
+    <br /></td>
+  </tr>
+ </table>
+</body>
+ 
+</html>
